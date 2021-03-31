e0f14cdeb911d9d5a749659c957a391713d95e55
Alina <alinasolnce99@gmail.com>
2021-03-31 17:10:30 +0300

    Finished with 5 repo

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const http = require('http');
-const routers = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-const server = http.createServer((req, res) => {
-    routers(req, res)
+const adminRouters = require('./routes/admin');
+const shopRouters = require('./routes/shop');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRouters);
+app.use(shopRouters);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "views", 'error.html'));
 })
-
-server.listen(3000);
+app.listen(3000);
