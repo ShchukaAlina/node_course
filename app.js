1e6fd89f690dddac1b216b9a796de10f748d668e
Alina <alinasolnce99@gmail.com>
2021-04-15 18:56:29 +0300

    added handlebars template

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const expressHbs= require('express-handlebars');
 
 const adminRouters = require('./routes/admin');
 const shopRouters = require('./routes/shop');
 
 const app = express();
-app.set("view engine", "pug");
+app.engine('hbs', expressHbs());
+app.set("view engine", "hbs"); //попробость сделать pug||hbs
 app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,6 +19,7 @@ app.use('/admin', adminRouters.routers);
 app.use(shopRouters);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", 'error.html'));
+    // res.status(404).sendFile(path.join(__dirname, "views", 'error.html'));
+    res.status(404).render("error", {docTitle: "Error"});
 })
 app.listen(3000);
